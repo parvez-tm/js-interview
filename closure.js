ae73a0a9b904f36c223f6e21bd62f21372586fd1
parvez-tm <parvez.tm@outlook.com>
2025-03-14 12:03:25 +0530

    few questions added

--- closure.js
+++ closure.js
@@ -5,6 +5,39 @@ function x (){
     }
 }
 
-console.log(x()) // This will return function
+//console.log(x()) // This will return function
 let z = x()
-z() // This will return 10
+//z() // This will return 10
+
+
+function outer() {
+    let count = 0;
+    return function inner() {
+      return ++count;
+    };
+  }
+  
+  const counter = outer();
+  
+//   console.log(counter()); // ?
+//   console.log(counter()); // ?
+//   console.log(counter()); // ?
+  
+
+
+function createFunctions() {
+    var arr = [];
+    for (let i = 0; i < 3; i++) {
+      arr.push(function () {
+        return i;
+      });
+    }
+    return arr;
+  }
+  
+  const funcs = createFunctions();
+  
+  console.log(funcs[0]()); // ?
+  console.log(funcs[1]()); // ?
+  console.log(funcs[2]()); // ?
+  
