ac7a64cdad07dd0cfe820e3e2ba931d98f9fc560
parvez-tm <parvez.tm@outlook.com>
2025-03-24 11:23:30 +0530

    while modification added

--- DSA/smallest-largest-number.js
+++ DSA/smallest-largest-number.js
@@ -1,6 +1,6 @@
 // Smallest Number
 
-let a = [-2, -8, -1, 0.7]
+let a = [-2, -8,-100, -1, 0.7]
 let num = a[0]
 
 if (a.length === 0) {
@@ -8,7 +8,7 @@ if (a.length === 0) {
 } else {
     // Using for loop 
     let num = a[0];
-    for (var i = 1; i < a.length; i++) {
+    for (let i = 1; i < a.length; i++) {
         if (num > a[i]) {
             num = a[i];
         }
@@ -19,5 +19,17 @@ if (a.length === 0) {
     // let num = a.reduce((min, value) => (value < min ? value : min), a[0]);
     // console.log(num);
 
+    // Usign While 
+
+    // let i = 0
+    // while (i < a.length) {
+    //     if (num > a[i]) {
+    //         num = a[i];
+    //     }
+    //     i++
+    // }
     console.log(num);
 }
+
+
+// for largest just reverse the condition form < to >
