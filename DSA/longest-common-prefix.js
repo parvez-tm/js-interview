36ee4fa891194db34730322871ff9e88c9aa7530
parvez-tm <parvez.tm@outlook.com>
2025-04-17 15:08:21 +0530

    in progress

--- DSA/longest-common-prefix.js
+++ DSA/longest-common-prefix.js
@@ -1,19 +1,30 @@
-strs = ["flower","flow","flight"]
+strs = ["ab", "a"]
 
 var longestCommonPrefix = function(strs) {
-    let val = ""
-    for (let i = 0; i < strs.length; i++) {
-        console.log(strs[i]);
+    let val = strs[0]
+    for (let i = 1; i < strs.length; i++) {
         for (let j = 0; j < strs[i].length; j++) {
-            if(strs[i][j] == strs[i+1][j]){
-                val += strs[i][j]
-                console.log(val,"a");
-            }
+            console.log(val[j], strs[i][j]);
+            // if(val[j] != strs[i][j]){
+            //     val = removeLastChar(val)
+            //     return
+            // }
         }
-        return
     }
-    console.log(val);
     return val
 };
 
 longestCommonPrefix(strs)
+
+
+function removeChar(s1,s2){
+
+}
+
+function removeLastChar(str) {
+    let result = '';
+    for (let i = 0; i < str.length - 1; i++) {
+      result += str[i];
+    }
+    return result;
+}
