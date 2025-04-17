1eebd141662c32f4ed0039e97ac32a25d38ca081
parvez-tm <parvez.tm@outlook.com>
2025-04-17 15:22:10 +0530

    one step clear

--- DSA/longest-common-prefix.js
+++ DSA/longest-common-prefix.js
@@ -2,14 +2,13 @@ strs = ["ab", "a"]
 
 var longestCommonPrefix = function(strs) {
     let val = strs[0]
-    for (let i = 1; i < strs.length; i++) {
-        for (let j = 0; j < strs[i].length; j++) {
-            console.log(val[j], strs[i][j]);
+    for (let i = 1; i < strs.length-1; i++) {
+        // for (let j = 0; j < strs[i].length; j++) {
             // if(val[j] != strs[i][j]){
             //     val = removeLastChar(val)
             //     return
             // }
-        }
+        // }
     }
     return val
 };
@@ -18,7 +17,14 @@ longestCommonPrefix(strs)
 
 
 function removeChar(s1,s2){
-
+    let result = ""
+    for (let i = 0; i < s1.length; i++) {
+        if(s1[i] == s2[i]){
+            result += s1[i]
+        }
+    }
+    // console.log(result);
+    return result
 }
 
 function removeLastChar(str) {
