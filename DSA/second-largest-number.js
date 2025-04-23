e47d31cdd058d7ef48029e7295e678fc9bc2f4a8
parvez-tm <parvez.tm@outlook.com>
2025-04-23 15:35:46 +0530

    Have to learn infinite in dept

---
+++ DSA/second-largest-number.js
@@ -0,0 +1,39 @@
+let a = [23,23,34,4,2,2,43,23,3]
+
+// My Logic
+    for (let i = 0; i < a.length; i++) {
+      for (let j = i; j < a.length; j++) {
+        if(a[i] > a[j]){
+          let b = a[i]
+          a[i] = a[j]
+          a[j] = b
+        }
+      }
+    }
+    
+    let b = a[a.length -1]
+    for (let i = a.length -1; i > 0; i--) {
+      // console.log(a[i],a[i] > b)
+      if(a[i]<b){
+      console.log(a[i],"ad")
+      break
+      }
+    }
+
+// GPT Solution without sort 
+function secondLargest(arr) {
+    let first = -Infinity, second = -Infinity;
+
+    for (let num of arr) {
+        if (num > first) {
+            second = first;
+            first = num;
+        } else if (num > second && num < first) {
+            second = num;
+        }
+    }
+
+    return second === -Infinity ? null : second;
+}
+console.log(secondLargest(a))
+// console.log(a,"Hello, World!");
