9154d97c3faf788c1aeba50e6c05d81ac4cc34f1
parvez-tm <parvez.tm@outlook.com>
2025-05-09 11:48:35 +0530

    few notes in linked list

--- DSA/linked-list.js
+++ DSA/linked-list.js
@@ -1,3 +1,13 @@
+// Notes:
+  // There are 3 ways to insert node in linked list:
+  // 1) start 
+  // 2) end
+  // 3) index
+  // Further there are two types of index:
+  // 1) start from 0
+  // 2) start from 1
+
+
 // Starting with minimal example
 const n1 = {
     data: 100
@@ -9,7 +19,8 @@ const n2 = {
 
 n1.next = n2
 
-//console.log(n1) //Output: { data: 100, next: { data: 200 } }
+// Output: { data: 100, next: { data: 200 } }: simple linked list
+// console.log(n1)
 
 class Node {
     constructor(value) {
